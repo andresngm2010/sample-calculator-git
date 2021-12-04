bd092794f2d0f64d31e75c602a2df00fa6a52626
Andres Gomez <andres.gomez@epn.edu.ec>
2021-12-04 18:47:58 -0500

    Commit Modification CalculatorExecute class Packaged again the .jar archive

--- src/main/java/ec/epn/edu/calculator/CalculatorExecute.java
+++ src/main/java/ec/epn/edu/calculator/CalculatorExecute.java
@@ -9,5 +9,23 @@ public class CalculatorExecute {
         System.out.println("c.addition(4,7) = "+c.addition(4,7));
 
         System.out.println("c.subtraction(9,2) = "+c.subtraction(9,2));
+
+        System.out.println("== Here begin the changes ==");
+
+        System.out.println("c.division(8,2) = "+c.division(8,2));
+
+        System.out.println("c.multiplication(6,4) = "+c.multiplication(6,4));
+
+        System.out.println("== Print Multiplication Table of 6");
+        System.out.println("c.multiplication(6,1) = "+c.multiplication(6,1));
+        System.out.println("c.multiplication(6,2) = "+c.multiplication(6,2));
+        System.out.println("c.multiplication(6,3) = "+c.multiplication(6,3));
+        System.out.println("c.multiplication(6,4) = "+c.multiplication(6,4));
+        System.out.println("c.multiplication(6,5) = "+c.multiplication(6,5));
+        System.out.println("c.multiplication(6,6) = "+c.multiplication(6,6));
+        System.out.println("c.multiplication(6,7) = "+c.multiplication(6,7));
+        System.out.println("c.multiplication(6,8) = "+c.multiplication(6,8));
+        System.out.println("c.multiplication(6,9) = "+c.multiplication(6,9));
+
     }
 }
